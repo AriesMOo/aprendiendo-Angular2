fe38ac859a4a35c8776f2ec5f2a3e9ca3067086b
Miguel (SACYL) <ariesmoo@gmail.com>
2016-10-21 14:48:30 +0200

    Retoque promesa (para simular lentitud en conexion)

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,7 +5,22 @@ import { HEROES } from '../app/mock-heroes';
 
 @Injectable()
 export class HeroService {
+
   getHeroes(): Promise<Hero[]> {
+    // Resolvemos la promesa y enviamos el array tal cual de objetos HEROES
     return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    // Patron encadenadmiento metodos (cada metodo devuelve el propio objeto
+    // con 'return this') y asi se puede volver a llamar a otro metodo de la
+    // misma clase una y otra vez)
+    //
+    // Las Promesas tienen la siguiente forma (simplificada):
+    // new Promise<tipo>().then().catch()
+    return new Promise<Hero[]>( resolve =>
+        setTimeout(resolve, 2000) )
+      .then( () => this.getHeroes() )
+      .catch( (error) => console.log(error) );
   }
 }
